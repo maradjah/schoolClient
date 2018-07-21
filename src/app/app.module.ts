8fbd40aba3511bb4036118ef3a75120cb262d569
maradjah <erradja.marwane@gmail.com>
2018-07-21 17:09:06 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -13,4 +14,6 @@ import { AppComponent } from './app.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+//comment
+}
